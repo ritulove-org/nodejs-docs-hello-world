7128346cc1834c7e6334b5128d02cf5f704c2e48
ritulove <31220997+ritulove@users.noreply.github.com>
2019-11-20 16:02:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
 
 });
-    response.end("Hello World from jenkins and Docker!");
+
+response.end("Hello World from jenkins and Docker!");
 const port = process.env.PORT || 1337;
 server.listen(port);
 
